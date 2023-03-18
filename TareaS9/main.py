c27a03b2e2234b1c6223fe6461fbe6d6bd56cf5f
Juan Jose <juanjo5792@gmail.com>
2023-03-18 12:38:42 -0500

    Homework Sesion #9

---
+++ TareaS9/main.py
@@ -0,0 +1,15 @@
+from func import calculate, random_function
+
+
+def main():
+    print("==================== Start Random Function  ====================")
+    random_function()
+    print("==================== End Random Function  ====================")
+
+    print("==================== Start Calc Function  ====================")
+    calculate()
+    print("==================== End Calc Function  ====================")
+
+
+if __name__ == "__main__":
+    main()
---
+++ TareaS9/utils/__init__.py
@@ -0,0 +1,5 @@
+import math
+
+def is_pair(number): return number % 2 == 0
+def custom_radians_to_degress(radians): return radians * 180 / math.pi
+def custom_degress_to_radians(degress): return degress * math.pi / 180
